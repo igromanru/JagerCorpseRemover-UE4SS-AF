7598c855c02d691702e08998f90484bc226c3646
igromanru <igromanru@yahoo.de>
2024-09-08 19:22:41 +0200

    feat: Add check if Actor destuction is pending feat: Check in 1sec loop for corpses if LevelName is "Facility_Office1"

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -8,7 +8,7 @@
 -- Don't change code below --
 ------------------------------
 
-require("AFUtils.AFUtils.BaseUtils")
+local AFUtils = require("AFUtils.AFUtils")
 
 ModName = "JagerCorpseRemover"
 ModVersion = "1.1.3"
@@ -38,12 +38,14 @@ local function FindAndRemoveJagerCorpse()
         for _, humanNPC in ipairs(humanNPCs) do
             ---@cast humanNPC ANarrativeNPC_Human_ParentBP_C
             if humanNPC.IsDead and GetJagerName() ~= NAME_None and humanNPC.NarrativeNPC_ConversationRow.RowName == GetJagerName() then
-                LogDebug("Found dead Jager, remove")
-                ExecuteInGameThread(function()
-                    humanNPC:SetActorHiddenInGame(true)
-                    humanNPC:SetActorEnableCollision(false)
-                    humanNPC:K2_DestroyActor()
-                end)
+                if not humanNPC.bActorIsBeingDestroyed then
+                    LogDebug("Found dead Jager, remove")
+                    ExecuteInGameThread(function()
+                        humanNPC:SetActorHiddenInGame(true)
+                        humanNPC:SetActorEnableCollision(false)
+                        humanNPC:K2_DestroyActor()
+                    end)
+                end
                 return true
             end
         end
@@ -60,12 +62,14 @@ local function FindAndRemoveKitchenCorpse()
         for _, actor in ipairs(skeletalMeshActors) do
             local location = actor:K2_GetActorLocation()
             if NearlyEqual(location.X, KitchenCorpseLocation.X) and NearlyEqual(location.Y, KitchenCorpseLocation.Y) and NearlyEqual(location.Z, KitchenCorpseLocation.Z) then
-                LogDebug("Found kitchen corpse, remove")
-                ExecuteInGameThread(function()
-                    actor:SetActorHiddenInGame(true)
-                    actor:SetActorEnableCollision(false)
-                    actor:K2_DestroyActor()
-                end)
+                if not actor.bActorIsBeingDestroyed then
+                    LogDebug("Found kitchen corpse, remove")
+                    ExecuteInGameThread(function()
+                        actor:SetActorHiddenInGame(true)
+                        actor:SetActorEnableCollision(false)
+                        actor:K2_DestroyActor()
+                    end)
+                end
                 return true
             end
         end
@@ -75,61 +79,16 @@ local function FindAndRemoveKitchenCorpse()
 end
 
 if IsModEnabled then
-    LoopAsync(800, function ()
-        FindAndRemoveJagerCorpse()
-        FindAndRemoveKitchenCorpse()
+    LoopAsync(1000, function ()
+        local playerController = AFUtils.GetMyPlayerController()
+        if playerController and playerController.ActiveLevelName:ToString() == "Facility_Office1" then
+            FindAndRemoveJagerCorpse()
+            FindAndRemoveKitchenCorpse()
+        end
         return false
     end)
 else
     LogInfo("The mod is disabled")
 end
-
--- local IsJagerCorpseLoopRunning = false
--- local function RunFindAndRemoveJagerCorpseLoop()
---     if IsJagerCorpseLoopRunning then
---         LogDebug("Jager Corpse Loop is already running")
---     else
---         LogDebug("Starting Jager Corpse Loop")
---         IsJagerCorpseLoopRunning = true
---         LoopAsync(800, function()
---             if FindAndRemoveJagerCorpse() then
---                 IsJagerCorpseLoopRunning = false
---                 return true
---             end
---             return false
---         end)
---     end
--- end
-
--- local IsKitchenCorpseLoopRunning = false
--- local function RunFindAndRemoveKitchenCorpseLoop()
---     if IsKitchenCorpseLoopRunning then
---         LogDebug("Kitchen Corpse Loop is already running")
---     else
---         LogDebug("Starting Kitchen Corpse Loop")
---         IsKitchenCorpseLoopRunning = true
---         LoopAsync(800, function()
---             if FindAndRemoveKitchenCorpse() then
---                 IsKitchenCorpseLoopRunning = false
---                 return true
---             end
---             return false
---         end)
---     end
--- end
-
--- RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(Context, NewPawn)
---     -- local playerController = Context:get()
---     -- local pawn = NewPawn:get()
-
---     -- LogDebug("----- [ClientRestart] called -----")
---     local gameState = GetGameState()
---     if gameState and gameState.MatchState == GetNameWaitingToStart() then
---         LogDebug("Starting find and remove loops")
---         RunFindAndRemoveJagerCorpseLoop()
---         RunFindAndRemoveKitchenCorpseLoop()
---     end
---     -- LogDebug("------------------------------")
--- end)
 
 LogInfo("Mod loaded successfully")
